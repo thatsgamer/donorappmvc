0ed6d8bbf91995f663f6c0a342aff590ffeace94
thatsgamer <susheelkasab@gmail.com>
2018-02-01 20:05:59 +0530

    February Build

---
+++ DonorAppVersion2/Models/MedicalReportUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DonorAppVersion2.Models
+{
+    public class MedicalReportUpdate
+    {
+        [Key]
+        public int MedicalReportId { get; set; }
+        public int DonorCycleId { get; set; }
+        public string UpdateHeading { get; set; }
+        public string UpdateDescription { get; set; }
+        public DateTime DateOfSubmission { get; set; }
+        public string Status { get; set; }
+
+        public virtual List<MedicalReportsQuestions> MedicalReportsQuestions { get; set; }
+    }
+}
